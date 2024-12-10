0f943c144c5c2b937a2476d25e9250c0530eba75
Ippat <nathan.ly.study@gmail.com>
2024-12-10 13:54:23 +1100

    cleaned up the code/display

--- modules/dom.js
+++ modules/dom.js
@@ -1,41 +1,8 @@
-/*Things to do
-Initialise game - Maybe a welcome screen?
-
-
-On click a letter 
-- check if letter has already been selected
-    - If so, display some sort of error
-
-- Check if letter is in hangman word
-    - If it is, reveal location, update object
-    - If not, update image, update object
-- Update "keyboard" based off key press
-
-- Check game over condition
-    - 0 lives: lose
-    - 0 hidden letters: win
-        Display game over
-
-Loop back to letter click if game in progress
-
-If game is finished, prompt for another game
-Add last game's word to used word
-Remove last game's word from word bank
-Display last game's word somewhere
-
-Resetting Game
-Create a new object/renew values for existing object?
-
-
-*/
-
 const hangmanDrawings = document.getElementById('hangmanWrapper');
-// console.log(hangmanDrawings.children[0]);
 
 export const createEl = (parent, child, text, id) => {
   const el = document.createElement(child);
   const pText = document.createTextNode(text);
-  //   console.log(text);
   el.appendChild(pText);
   el.id = id;
   parent.appendChild(el);
@@ -47,7 +14,6 @@ export const updateEl = (element, text) => {
 
 export const updatePicture = (game) => {
   const progress = game.wrongGuesses;
-  //   console.log(hangmanDrawings.children[progress]);
   hangmanDrawings.children[progress].classList.remove('invisible');
   hangmanDrawings.children[progress].classList.add('visible');
 };
@@ -55,7 +21,6 @@ export const updatePicture = (game) => {
 export const resetPicture = () => {
   const hangmanWrapper = document.querySelector('#hangmanWrapper');
   const hangmen = hangmanWrapper.querySelectorAll('div');
-  //   console.log(hangmen);
   hangmen.forEach((drawing) => {
     drawing.classList.remove('visible');
     drawing.classList.add('invisible');
--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-import { newGame, wordBank, prevWords, resetGame } from './modules/backend.js';
-import { gameMove, updateHistory } from './modules/backend.js';
+import {
+  newGame,
+  wordBank,
+  prevWords,
+  resetGame,
+  gameMove,
+  updateHistory,
+} from './modules/backend.js';
 import {
   createEl,
   updateEl,
@@ -8,14 +14,11 @@ import {
 } from './modules/dom.js';
 
 const game = newGame(wordBank);
-// console.log(game);
-// const hangmanPic = document.getElementById('hangmanPicture');
 const hangmanWord = document.getElementById('hangmanWord');
-const prevDisplay = document.getElementById('prevWordDisplay');
+const prevWordContainer = document.getElementById('prevWordDisplay');
+const prevDisplay = document.getElementById('previousWords');
 const prevButton = document.getElementById('prevButton');
 const newGameButton = document.getElementById('newGameButton');
-// const altHangmanWord = document.querySelector('#hangmanWord');
-// console.log(hangmanWord);
 createEl(hangmanWord, 'p', game.correctLetters.join(' '), 'hangmanWordDisplay');
 createEl(
   hangmanWord,
@@ -26,27 +29,18 @@ createEl(
 const hangmanWordDisp = document.getElementById('hangmanWordDisplay');
 const hangmanLivesDisp = document.getElementById('hangmanLivesDisplay');
 const allButtons = document.querySelectorAll('.keyboard__button');
-// console.log(wordBank);
 allButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    // const letter = button.innerText.toLowerCase();
     const gameOver = gameMove(game, button);
-    // if (gameStatus) {do something}
     if (gameOver) {
-      // console.log(game.word);
       updateEl(hangmanWordDisp, game.word.join(' '));
       updateEl(hangmanLivesDisp, gameOver);
       updateHistory(wordBank, prevWords);
-      updateEl(prevDisplay, prevWords);
+      updateEl(prevDisplay, prevWords.join(', '));
       newGameButton.classList.remove('invisible');
       allButtons.forEach((button) => {
         button.disabled = true;
       });
-      // console.log(prevWords);
-      //display word
-      //disable all buttons
-      //add to word history
-      //prompt rematch
     } else {
       updateEl(hangmanWordDisp, game.correctLetters.join(' '));
       updateEl(hangmanLivesDisp, `Lives Remaining: ${10 - game.wrongGuesses}`);
@@ -55,10 +49,10 @@ allButtons.forEach((button) => {
 });
 
 prevButton.addEventListener('click', () => {
-  prevDisplay.classList.add('visible');
+  prevWordContainer.classList.add('visible');
 });
-prevDisplay.addEventListener('click', () => {
-  prevDisplay.classList.remove('visible');
+prevWordContainer.addEventListener('click', () => {
+  prevWordContainer.classList.remove('visible');
 });
 
 newGameButton.addEventListener('click', () => {
